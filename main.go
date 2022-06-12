06bf68769975b028ff8863c56d56a0b0235e2281
Nelson Estevão <nelsonmestevao@proton.me>
2022-06-12 14:02:13 +0100

    Add basic functionality

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 )
 
+func count(r io.Reader) int {
+	scanner := bufio.NewScanner(r)
+
+	scanner.Split(bufio.ScanWords)
+
+	wc := 0
+
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(count(os.Stdin))
 }
